d0bf7391d4616d3e851491b3903ecf8cd60ccdb3
danielrpg <daniel.fernandezgp@gmail.com>
2019-08-31 20:40:52 -0400

    Insert and Remove changes

--- app/src/main/java/com/app/recyclerview_cardview/MainActivity.java
+++ app/src/main/java/com/app/recyclerview_cardview/MainActivity.java
@@ -5,31 +5,66 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<Item> itemList;
+
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    private Button btnInsert, btnRemove;
+    private EditText etInsert, etRemove;
+
+    private static final String MSG_ADDED_TITLE = "New line";
+    private static final String MSG_ADDED = "The new Item added ";
+    private static final String MSG_ADDED_SUCCESS = "Your item was successfully added!";
+    private static final String MSG_REMOVED = "Your Item was successfully removed!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Item> itemList = new ArrayList<>();
+        createItemList();
+        buildRecyclerView();
+
+        btnInsert = findViewById(R.id.button_insert);
+        btnRemove = findViewById(R.id.button_remove);
+
+        etInsert = findViewById(R.id.edittext_insert);
+        etRemove = findViewById(R.id.edittext_remove);
+    }
+
+    public void insertItem(View v) {
+        int position = Integer.parseInt(etInsert.getText().toString());
+        itemList.add(position, new Item(R.drawable.ic_android, MSG_ADDED_TITLE + position, MSG_ADDED + position ));
+        adapter.notifyItemInserted(position);
+        Toast.makeText(this, MSG_ADDED_SUCCESS, Toast.LENGTH_LONG).show();
+    }
+
+    public void removeItem(View v) {
+        int position = Integer.parseInt(etRemove.getText().toString());
+        itemList.remove(position);
+        adapter.notifyItemRemoved(position);
+        Toast.makeText(this, MSG_REMOVED, Toast.LENGTH_LONG).show();
+    }
+
+    public void createItemList() {
+        itemList = new ArrayList<>();
         itemList.add(new Item(R.drawable.ic_android, "Line 1", "Line 2"));
         itemList.add(new Item(R.drawable.ic_audio, "Line 3", "Line 4"));
         itemList.add(new Item(R.drawable.ic_sun, "Line 5", "Line 6"));
-        itemList.add(new Item(R.drawable.ic_android, "Line 7", "Line 8"));
-        itemList.add(new Item(R.drawable.ic_audio, "Line 9", "Line 10"));
-        itemList.add(new Item(R.drawable.ic_sun, "Line 11", "Line 12"));
-        itemList.add(new Item(R.drawable.ic_android, "Line 13", "Line 14"));
-        itemList.add(new Item(R.drawable.ic_audio, "Line 15", "Line 16"));
-        itemList.add(new Item(R.drawable.ic_sun, "Line 17", "Line 18"));
+    }
 
+    public void buildRecyclerView() {
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
